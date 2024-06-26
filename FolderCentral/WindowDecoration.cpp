35da50eed080b2d7313cd5195889627eeac8c136
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2024-06-26 10:58:07 -0600

    WANT TO COMMIT SUICIDE AAAAAAAAAAAAA

--- FolderCentral/WindowDecoration.cpp
+++ FolderCentral/WindowDecoration.cpp
@@ -14,6 +14,7 @@ void fcw::kill_all_children(wxPanel* panel)
 
 void fcw::decorate_left_panel(wxPanel* left_panel)
 {
+    left_panel->SetBackgroundColour(wxColor(38, 113, 15));
     wxSizer* panel_sizer = new wxBoxSizer(wxVERTICAL);
     std::string file_path = "../configuration/saved_subroutines_directories.txt";
 
@@ -43,6 +44,10 @@ void fcw::decorate_left_panel(wxPanel* left_panel)
 
     for (std::string directory : directories)
     {
+        if (directory.empty())
+        {
+            continue;
+        }
         std::filesystem::path path(directory);
         wxButton* button = new wxButton(left_panel, wxID_ANY, path.filename().string(), wxDefaultPosition, wxSize(250, 30));
         panel_sizer->Add(button, 0, wxEXPAND | wxALL, 0);
@@ -50,9 +55,9 @@ void fcw::decorate_left_panel(wxPanel* left_panel)
 
     wxWindow* root = left_panel->GetParent();
     wxButton* new_folder_button = new wxButton(left_panel, wxID_ANY, "+", wxDefaultPosition, wxSize(250, 40));
-    new_folder_button->Bind(wxEVT_BUTTON, [root](wxCommandEvent& event)
+    new_folder_button->Bind(wxEVT_BUTTON, [root, left_panel](wxCommandEvent& event)
         {
-            fcw::add_folder(event, root);
+            fcw::add_folder_window(event, root, left_panel);
         });
     panel_sizer->Add(new_folder_button, 0, wxEXPAND | wxALL, 0);
 
@@ -61,7 +66,7 @@ void fcw::decorate_left_panel(wxPanel* left_panel)
 }
 
 
-void fcw::add_folder(wxCommandEvent& event, wxWindow* root)
+void fcw::add_folder_window(wxCommandEvent& event, wxWindow* root, wxPanel* left_panel)
 {
     wxWindowList children = root->GetChildren();
     for (wxWindowList::iterator it = children.begin(); it != children.end(); ++it)
@@ -74,13 +79,63 @@ void fcw::add_folder(wxCommandEvent& event, wxWindow* root)
     }
 
     wxFrame* dialog_frame = new wxFrame(root, wxID_ANY, "Add a new folder");
-    dialog_frame->SetMinSize(wxSize(300, 100));
-    dialog_frame->SetMaxSize(wxSize(300, 100));
+    dialog_frame->SetMinSize(wxSize(450, 160));
+    dialog_frame->SetMaxSize(wxSize(450, 160));
+
+    wxStatusBar* status = new wxStatusBar(dialog_frame, wxID_ANY);
+    dialog_frame->SetStatusBar(status);
 
     wxPanel* panel = new wxPanel(dialog_frame, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+    panel->SetBackgroundColour(wxColor(200, 100, 200));
+
+    wxTextCtrl* entry = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(20, 15), wxSize(390, 20));
+    entry->SetMaxLength(100);
+
+    wxButton* add_button = new wxButton(panel, wxID_ANY, "Add", wxPoint(20, 55));
+    add_button->Bind(wxEVT_BUTTON, [status, entry, left_panel](wxCommandEvent& event)
+        {
+            fcw::add_folder_button(event, status, entry, left_panel);
+        });
+
+    wxButton* search_button = new wxButton(panel, wxID_ANY, "Search", wxPoint(335, 55));
+    add_button->Bind(wxEVT_BUTTON, [dialog_frame, entry](wxCommandEvent& event)
+        {
+            fcw::search_folder_path(event, dialog_frame, entry);
+        });
 
     dialog_frame->Center();
     dialog_frame->Show();
 }
 
+void fcw::add_folder_button(wxCommandEvent& event, wxStatusBar* status, wxTextCtrl* entry, wxPanel* left_panel)
+{
+    std::string path = entry->GetValue().ToStdString();
+    std::filesystem::path new_folder(fcf::raw_path(path));
+    if (!(std::filesystem::exists(path) && std::filesystem::is_directory(path)))
+    {
+        status->SetStatusText(wxString("Invalid path!"));
+        entry->Clear();
+        return;
+    }
 
+    std::ofstream file;
+    file.open("../configuration/saved_subroutines_directories.txt", std::ios_base::app);
+    file << path << "\n";
+    file.close();
+
+    fcw::kill_all_children(left_panel);
+    fcw::decorate_left_panel(left_panel);
+}
+
+
+void fcw::search_folder_path(wxCommandEvent& event, wxFrame* frame, wxTextCtrl* entry)
+{
+    wxDirDialog* folder_dialog = new wxDirDialog(frame);
+    
+    
+    if (folder_dialog->ShowModal() == wxID_OK)
+    {
+        wxString path = folder_dialog->GetPath();
+        entry->SetValue(path);
+    }
+}
--- FolderCentral/WindowDecoration.h
+++ FolderCentral/WindowDecoration.h
@@ -12,9 +12,9 @@ namespace fcw
 {
     void kill_all_children(wxPanel* panel);
     void decorate_left_panel(wxPanel* left_panel);
-    void add_folder(wxCommandEvent& event, wxWindow* root);
-    
-
+    void add_folder_window(wxCommandEvent& event, wxWindow* root, wxPanel* left_panel);
+    void add_folder_button(wxCommandEvent& event, wxStatusBar* status, wxTextCtrl* entry, wxPanel* left_panel);
+    void search_folder_path(wxCommandEvent& event, wxFrame* frame, wxTextCtrl* entry);
 }
 
 #endif
